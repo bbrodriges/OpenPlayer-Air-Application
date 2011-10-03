3d4cafd27fb4c2387e6cc9bcfe612f6912902c46
bbrodriges <bender@rodriges.org>
2011-10-03 08:12:57 +0400

    Initial Api.php extension commit

---
+++ Api.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App;
+
+use \Lib\Request;
+
+class Api extends \Lib\Base\App {
+
+    public function init() {
+    	if(Request::get('method')) {
+			switch(Request::get('method')) {
+				case 'search':
+					$result = \Lib\AudioParser::search($_GET['query'], 0);
+					echo json_encode($result);
+					die;
+					break;
+					
+				case 'getsound':
+					$url = Request::get('url');
+					
+					$ch = curl_init($url);
+					curl_setopt($ch, CURLOPT_NOBODY, true);
+					curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+					curl_setopt($ch, CURLOPT_HEADER, true);
+					$data = curl_exec($ch);
+					curl_close($ch);
+			
+					if (preg_match('/Content-Length: (\d+)/', $data, $matches)) {
+						$contentLength = (int)$matches[1];
+					}
+					
+					header('Last-Modified:');
+					header('ETag:');
+					header('Content-Type: audio/mpeg');
+					header('Accept-Ranges: bytes');
+					header('Content-Length: '.$contentLength);
+					
+					set_time_limit(0);
+					$ch = curl_init($url);
+					curl_setopt($ch, CURLOPT_BINARYTRANSFER, 1);
+					curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+					$song = curl_exec($ch);
+					curl_close($ch);
+			        echo $song;
+					die;
+					break;
+					
+				default:
+					die;
+					break;
+			}
+    	}
+    }
+    
+    private function returnSound($url) {
+		//getFileSize($url);
+		/*set_time_limit(0);
+		$ch = curl_init($url);
+		curl_setopt($ch, CURLOPT_BINARYTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		$song = curl_exec($ch);
+		curl_close($ch);
+        echo $song;*/
+    }
+	
+	private function getFileSize($url) {
+		/*$ch = curl_init($url);
+		curl_setopt($ch, CURLOPT_NOBODY, true);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_HEADER, true);
+		$data = curl_exec($ch);
+		curl_close($ch);
+
+		if (preg_match('/Content-Length: (\d+)/', $data, $matches)) {
+			$contentLength = (int)$matches[1];
+		}*/
+		
+		header('Last-Modified:');
+		header('ETag:');
+		header('Content-Type: audio/mpeg');
+		header('Accept-Ranges: bytes');
+		header('Content-Length: '.$contentLength);
+	}
+
+}
+
+?>
